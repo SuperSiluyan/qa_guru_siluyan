0f757233ea977e5788f2dd9fe308b228e3a88671
siluy <siluyan_and@mail.ru>
2023-09-11 21:30:42 +0300

    uzhas

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,8 +1,9 @@
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestForm {
@@ -24,6 +25,35 @@ public class TestForm {
         $("[class='react-datepicker__day react-datepicker__day--005']").click();
         $("[id='subjectsInput']").setValue("Maths");
         $("[id=react-select-2-option-0]").click();
+        $("[for=hobbies-checkbox-1]").click();
+        $("[id=uploadPicture]").sendKeys("C:\\b.jpg");
+        $("[id=currentAddress]").setValue("Russia Yaroslavl");
+       ///$x("//class[contains@text()='State']").click();
+       $("[id=state]").click();
+        $x("//*[.='NCR']").click();
+        $("[id=city]").click();
+        $x("//*[.='Delhi']").click();
+        $("[id=submit]").click();
+
+       $("[class=modal-content]").shouldHave(text("Andrey Siluyanov"));
+        $("[class=modal-content]").shouldHave(text("Siluyan_and@mail.ru"));
+        $("[class=modal-content]").shouldHave(text("Male"));
+        $("[class=modal-content]").shouldHave(text("9011111111"));
+        $("[class=modal-content]").shouldHave(text("05 March,1991"));
+        $("[class=modal-content]").shouldHave(text("Maths"));
+        $("[class=modal-content]").shouldHave(text("Sports"));
+        $("[class=modal-content]").shouldHave(text("b.jpg"));
+        $("[class=modal-content]").shouldHave(text("Russia Yaroslavl"));
+        $("[class=modal-content]").shouldHave(text("NCR Delhi"));
+
+
+
+
+
+
+
+
+
 
     }
 }
