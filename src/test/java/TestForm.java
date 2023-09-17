c9d8548e21c6fd77ebdf87cf70a47f88c38a6e56
siluy <siluyan_and@mail.ru>
2023-09-17 23:50:54 +0300

    fix

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,8 +1,7 @@
-import com.codeborne.selenide.CollectionCondition;
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,44 +13,46 @@ public class TestForm {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.holdBrowserOpen =true;
 
     }
 
     @Test
     void fillTestForm() {
 
-        open("https://demoqa.com/automation-practice-form");
+        open("automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Andrey");
-        $("[id=userEmail]").setValue("Siluyan_and@mail.ru");
+        $("#lastName").setValue("Siluyanov");
+        $("#userEmail").setValue("Siluyan_and@mail.ru");
         $x("//label[@for='gender-radio-1']").click();
-        $("[id=userNumber]").setValue("9011111111");
-        $("[id=dateOfBirthInput]").click();
-        $("[class='react-datepicker__month-select']").selectOptionContainingText("March");
-        $("[class='react-datepicker__year-select']").selectOptionContainingText("1991");
-        $("[class='react-datepicker__day react-datepicker__day--005']").click();
-        $("[id='subjectsInput']").setValue("Maths");
-        $("[id=react-select-2-option-0]").click();
+        $("#userNumber").setValue("9011111111");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("March");
+        $(".react-datepicker__year-select").selectOptionContainingText("1991");
+        $(".react-datepicker__day react-datepicker__day--005").click();
+        $("#subjectsInput").setValue("Maths");
+        $("#react-select-2-option-0").click();
         $("[for=hobbies-checkbox-1]").click();
         $("#uploadPicture").uploadFromClasspath("b.jpg");
-        $("[id=currentAddress]").setValue("Russia Yaroslavl");
-        $("[id=state]").click();
+        $("#currentAddress").setValue("Russia Yaroslavl");
+        $("#state").click();
         $x("//*[.='NCR']").click();
-        $("[id=city]").click();
+        $("#city").click();
         $x("//*[.='Delhi']").click();
-        $("[id=submit]").click();
+        $("#submit").click();
 
         //Проверка текста
-        $("[class=modal-content]").shouldHave(text("Andrey Siluyanov"));
-        $("[class=modal-content]").shouldHave(text("Siluyan_and@mail.ru"));
-        $("[class=modal-content]").shouldHave(text("Male"));
-        $("[class=modal-content]").shouldHave(text("9011111111"));
-        $("[class=modal-content]").shouldHave(text("05 March,1991"));
-        $("[class=modal-content]").shouldHave(text("Maths"));
-        $("[class=modal-content]").shouldHave(text("Sports"));
-        $("[class=modal-content]").shouldHave(text("b.jpg"));
-        $("[class=modal-content]").shouldHave(text("Russia Yaroslavl"));
-        $("[class=modal-content]").shouldHave(text("NCR Delhi"));
+        $(".modal-content").shouldHave(text("Andrey Siluyanov"));
+        $(".modal-content").shouldHave(text("Siluyan_and@mail.ru"));
+        $(".modal-content").shouldHave(text("Male"));
+        $(".modal-content").shouldHave(text("9011111111"));
+        $(".modal-content").shouldHave(text("05 March,1991"));
+        $(".modal-content").shouldHave(text("Maths"));
+        $(".modal-content").shouldHave(text("Sports"));
+        $(".modal-content").shouldHave(text("b.jpg"));
+        $(".modal-content").shouldHave(text("Russia Yaroslavl"));
+        $(".modal-content").shouldHave(text("NCR Delhi"));
     }
 }
