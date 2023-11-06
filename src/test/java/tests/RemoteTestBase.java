f27cf4f45b30ba0be2cf11b1f5209f16178bf676
siluy <siluyan_and@mail.ru>
2023-11-06 20:37:27 +0300

    fix RemoteTestBase

--- src/test/java/tests/RemoteTestBase.java
+++ src/test/java/tests/RemoteTestBase.java
@@ -2,11 +2,15 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class RemoteTestBase {
     @BeforeAll
@@ -30,5 +34,14 @@ public class RemoteTestBase {
         Configuration.browserCapabilities = capabilities;
     }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
     }
 }
+
